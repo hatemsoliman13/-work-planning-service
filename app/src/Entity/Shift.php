03781b44311b03b63fca3f2d75d513ffa923e08f
hatemsoliman13 <hatem.soliman333@gmail.com>
2022-07-15 08:26:35 +0200

    adding worker and shift
    - creating schemas for two tables (worker, shift)
    - adding entities for worker and shift
    - adding repositories for worker and shift

---
+++ app/src/Entity/Shift.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ShiftRepository;
+use App\Entity\Worker;
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\JoinColumn;
+
+/**
+ * @ORM\Entity(repositoryClass=ShiftRepository::class)
+ */
+class Shift
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ManyToOne(targetEntity="Worker", inversedBy="shifts")
+     * @JoinColumn(name="worker_id", referencedColumnName="id", nullable=false)
+     */
+    private $worker;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $shiftHours;
+
+    /**
+     * @ORM\Column(type="datetime", name="shift_date_time")
+     */
+    private $shiftDateTime;
+
+    /**
+     * @ORM\Column(type="datetime", name="create_date_time")
+     */
+    private $createDateTime;
+
+    /**
+     * @ORM\Column(type="datetime", name="update_date_time")
+     */
+    private $updateDateTime;
+
+    /**
+     * Get the value of createDateTime
+     */
+    public function getCreateDateTime(): DateTime
+    {
+        return $this->createDateTime;
+    }
+
+    /**
+     * Set the value of createDateTime
+     *
+     * @return  self
+     */
+    public function setCreateDateTime(DateTime $createDateTime)
+    {
+        $this->createDateTime = $createDateTime;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of updateDateTime
+     */
+    public function getUpdateDateTime(): DateTime
+    {
+        return $this->updateDateTime;
+    }
+
+    /**
+     * Set the value of updateDateTime
+     *
+     * @return  self
+     */
+    public function setUpdateDateTime(DateTime $updateDateTime)
+    {
+        $this->updateDateTime = $updateDateTime;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of shiftDateTime
+     */
+    public function getShiftDateTime(): DateTime
+    {
+        return $this->shiftDateTime;
+    }
+
+    /**
+     * Set the value of shiftDateTime
+     *
+     * @return  self
+     */
+    public function setShiftDateTime(DateTime $shiftDateTime)
+    {
+        $this->shiftDateTime = $shiftDateTime;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of shiftHours
+     */
+    public function getShiftHours(): string
+    {
+        return $this->shiftHours;
+    }
+
+    /**
+     * Set the value of shiftHours
+     *
+     * @return  self
+     */
+    public function setShiftHours(string $shiftHours)
+    {
+        $this->shiftHours = $shiftHours;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of worker
+     */
+    public function getWorker(): Worker
+    {
+        return $this->worker;
+    }
+
+    /**
+     * Set the value of worker
+     *
+     * @return  self
+     */
+    public function setWorker(Worker $worker): self
+    {
+        $this->worker = $worker;
+
+        return $this;
+    }
+}
---
+++ app/src/Entity/Worker.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WorkerRepository;
+use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\OneToMany;
+
+/**
+ * @ORM\Entity(repositoryClass=WorkerRepository::class)
+ */
+class Worker
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50, unique="true")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="datetime", name="create_date_time")
+     */
+    private $createDateTime;
+
+    /**
+     * @ORM\Column(type="datetime", name="update_date_time")
+     */
+    private $updateDateTime;
+
+    /**
+     * @OneToMany(targetEntity="Shift", mappedBy="worker", cascade={"remove"})
+     */
+    private $shifts;
+
+    public function __construct()
+    {
+        $this->shifts = new ArrayCollection();
+    }
+
+    /**
+     * Get the value of name
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set the value of name
+     *
+     * @return  self
+     */
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of createDateTime
+     */
+    public function getCreateDateTime(): DateTime
+    {
+        return $this->createDateTime;
+    }
+
+    /**
+     * Set the value of createDateTime
+     *
+     * @return  self
+     */
+    public function setCreateDateTime(DateTime $createDateTime): self
+    {
+        $this->createDateTime = $createDateTime;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of updateDateTime
+     */
+    public function getUpdateDateTime(): DateTime
+    {
+        return $this->updateDateTime;
+    }
+
+    /**
+     * Set the value of updateDateTime
+     *
+     * @return  self
+     */
+    public function setUpdateDateTime(DateTime $updateDateTime): self
+    {
+        $this->updateDateTime = $updateDateTime;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of shifts
+     */
+    public function getShifts(): Collection
+    {
+        return $this->shifts;
+    }
+
+    /**
+     * Set the value of shifts
+     *
+     * @return  self
+     */
+    public function setShifts(Collection $shifts): self
+    {
+        $this->shifts = $shifts;
+
+        return $this;
+    }
+}
