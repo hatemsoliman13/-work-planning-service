afa999a99bef55806cb13b6890fa066ebd53f9f0
hatemsoliman13 <hatem.soliman333@gmail.com>
2022-07-16 16:28:17 +0200

    adding shift controller and twig templates
    - creating shift controller to handle all the crud requests for the
    shifts
    - creating twig files to handle the crud frontend

---
+++ app/src/Controller/ShiftController.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Shift;
+use App\Entity\Worker;
+use App\Serive\ShiftService;
+use App\Serive\WorkerService;
+use DateTime;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ShiftController extends AbstractController
+{
+    /**
+     * @Route("/", name="index_shifts", methods={"GET"})
+     */
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $shiftRepo = $entityManager->getRepository(Shift::class);
+        $shifts = $shiftRepo->findBy([], ['shiftDateTime' => 'DESC']);
+
+        return $this->render('shifts/index.html.twig', ['shifts' => $shifts]);
+    }
+
+    /**
+     * @Route("/create", name="create_shift", methods={"POST", "GET"})
+     */
+    public function create(
+        EntityManagerInterface $entityManager,
+        Request $request
+    ): Response {
+
+        if ($request->getMethod() === 'GET') {
+            return $this->render('/shifts/create.html.twig');
+        }
+
+        $submittedToken = $request->request->get('token');
+
+        if (!$this->isCsrfTokenValid('create-shift', $submittedToken)) {
+            $this->addFlash('notice', 'Your request was refused');
+            return $this->redirectToRoute('index_shifts');
+        }
+
+        $worker = $entityManager->getRepository(Worker::class)->findOneBy([
+            'name' => $request->get('name')
+        ]);
+
+        if (empty($worker)) {
+            $workerService = new WorkerService($entityManager);
+            $worker = $workerService->create($request);
+        }
+
+        $shiftService = new ShiftService($entityManager);
+        $shiftService->create($worker, $request);
+
+        try {
+            $entityManager->flush();
+        } catch (UniqueConstraintViolationException $e) {
+            $this->addFlash(
+                'notice',
+                'Worker is not free!'
+            );
+            return $this->render('/shifts/create.html.twig');
+        }
+
+        $this->addFlash(
+            'notice',
+            'Created Successfully!'
+        );
+        return $this->redirectToRoute('index_shifts');
+    }
+
+    /**
+     * @Route("/show/{id}", name="show_shift", methods={"GET"})
+     */
+    public function show(EntityManagerInterface $entityManager, int $id): Response
+    {
+        $shift = $entityManager->find(Shift::class, $id);
+
+        return $this->render('shifts/show.html.twig', ['shift' => $shift]);
+    }
+
+    /**
+     * @Route("/update/{id}", name="update_shift", methods={"POST", "GET"})
+     */
+    public function update(
+        EntityManagerInterface $entityManager,
+        Request $request,
+        int $id
+    ): Response {
+        $shift = $entityManager->find(Shift::class, $id);
+
+        if ($request->getMethod() === 'GET') {
+            return $this->render('shifts/update.html.twig', ['shift' => $shift]);
+        }
+
+        $submittedToken = $request->request->get('token');
+
+        if (!$this->isCsrfTokenValid('update-shift', $submittedToken)) {
+            $this->addFlash(
+                'notice',
+                'Your request was refused'
+            );
+            return $this->redirectToRoute('index_shifts');
+        }
+
+        $workerService = new WorkerService($entityManager);
+        $workerService->update($shift->getWorker(), $request);
+
+        $shiftService = new ShiftService($entityManager);
+        $shiftService->update($shift, $request);
+
+        try {
+            $entityManager->flush();
+        } catch (UniqueConstraintViolationException $e) {
+            $this->addFlash(
+                'notice',
+                'Worker is not free!'
+            );
+            return $this->render(
+                'shifts/update.html.twig', ['shift' => $shift]
+            );
+        }
+
+        $this->addFlash(
+            'notice',
+            'Updated Successfully!'
+        );
+        return $this->redirectToRoute('index_shifts');
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete_shift", methods={"POST"})
+     */
+    public function delete(EntityManagerInterface $entityManager, Request $request, int $id): Response
+    {
+        $submittedToken = $request->request->get('token');
+
+        if (!$this->isCsrfTokenValid('delete-shift', $submittedToken)) {
+            $this->addFlash(
+                'notice',
+                'Your request was refused'
+            );
+            return $this->redirectToRoute('index_shifts');
+        }
+
+        $entityManager->remove($entityManager->find(Shift::class, $id));
+        $entityManager->flush();
+
+        return $this->redirectToRoute('index_shifts');
+    }
+}
